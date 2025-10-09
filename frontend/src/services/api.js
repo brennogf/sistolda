1fbf8856ee5169417bc1a009ff002f6b1ed99c06
root <root@G15.localdomain>
2025-10-09 15:26:35 -0300

    baseURL da API alterada

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:10000' })
+const api = axios.create({ baseURL: 'https://sistolda.onrender.com' })
 
 export default api
