b1f1b4c9f2a6fae921e27332edf22070563d7758
root <root@G15.localdomain>
2025-10-09 15:45:17 -0300

    Remoção total do cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import path from 'path'
-//import cors from "cors"
 const bodyParser = require('body-parser')
 const app = express()
 const routes = require("./routes")
@@ -8,8 +7,6 @@ const PORT = process.env.PORT || 4000
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
-//app.use(cors());
 
 app.use(express.json());
 app.use(routes);
