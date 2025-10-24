67a801d1543c149996be733b667ae68b1c64748b
brennogf <brennogivigier@gmail.com>
2025-10-24 01:51:37 -0300

    Add cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,12 @@ const PORT = process.env.PORT || 4000
 app.use(bodyParser.json({ limit: '10mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }))
 
-app.use(cors())
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(routes)
 
